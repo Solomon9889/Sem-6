43558958bf1b030eae8c464ae29c46df84d93286
Solomon <you@example.com>
2019-02-14 19:52:49 +0530

    Lexical Analysis code updated

--- SPCC/geek.c
+++ SPCC/geek.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
- 
 int main()
 {
-int c, n, fact = 1;
- 
-printf("Enter a number to calculate its factorial\n");
-scanf("%d", &n);
- 
-for (c = 1; c <= n; c++)
-fact = fact * c;
- 
-printf("Factorial of %d = %d\n", n, fact);
- 
-return 0;
+int n , i ;
+unsigned long long factorial = 1 ;
+
+printf ( " Enter an integer: " ) ;
+scanf (" %d ", &n ) ;
+
+    
+if ( n < 0 )
+printf ( " Error! Factorial of a negative number doesn't exist. " ) ;
+
+else
+{
+for ( i = 1 ; i <= n ; ++i )
+{
+factorial = factorial * i ;              
+}
+printf ( " Factorial of %d = %llu ", n , factorial ) ;
 }
 
+return 0 ;
+}
--- SPCC/lex.py
+++ SPCC/lex.py
@@ -1,42 +1,31 @@
 #!/usr/bin/python
 
-def eachlinek(line):
-	words = line.split(' ')+line.split('(')
-	
-	word = 0
-	for i in words:
-		if((i == 'for') or (i == 'if') or (i == 'else') or (i == 'while') or (i == 'do') or  (i == 'break') or  (i == 'continue') or (i == 'int') or (i == 'double') or (i == 'float') or (i == 'return') or (i == 'char') or (i == 'case') or (i == 'char') or (i == 'sizeof') or (i == 'long')or (i == 'short') or (i == 'typedef') or (i == 'switch') or (i == 'unsigned') or (i == 'void') or (i == 'static') or (i == 'struct') or (i == 'goto')):
-			word=word+1
-	
-	return word
-
-def eachlineo(line):
-	words = line.split(' ')
-	
-	word = 0
-	for i in words:
-		if((i == '+') or (i == '-') or (i == '*') or (i == '/') or (i == '%') or  ((i[1:3] == '++') or (i[0:2] == '++')) or ((i[1:3] == '--') or (i[0:2] == '--')) or (i == '<') or (i == '>') or (i == '<=') or (i == '>=') or (i == '=') ):
-			word=word+1
-	
-	return word
-
 
 def forkeyword(data):
-	total = 0
-	for i in data:
-		count = eachlinek(i)
-		total = total+ count
-	print' 1) Total number of keywords:',total
-
-def foroperator(data):
-	total = 0
-	for i in data:
-		count = eachlineo(i)
-		total = total+ count
-	print' 2) Total number of operators:',total
-
-
+	
+	
+	
+	keyword = 0
+	operators = 0
+	identifiers = 0
+	
+	for a in data:
+		print a
+		words = a.split(' ')
+		for i in words:
+			if((i == 'for') or (i == 'if') or (i == 'else') or (i == 'while') or (i == 'do') or  (i == 'break') or  (i == 'continue') or (i == 'int') or (i == 'double') or (i == 'float') or (i == 'return') or (i == 'char') or (i == 'case') or (i == 'char') or (i == 'sizeof') or (i == 'long')or (i == 'short') or (i == 'typedef') or (i == 'switch') or (i == 'unsigned') or (i == 'void') or (i == 'static') or (i == 'struct') or (i == 'goto')):
+				keyword=keyword+1
+			elif((i == '+') or (i == '-') or (i == '*') or (i == '/') or (i == '%') or  (i[1:3] == '++') or (i[0:2] == '++') or (i[1:3] == '--') or (i[0:2] == '--') or (i == '<') or (i == '>') or (i == '<=') or (i == '>=') or (i == '=') or (i == '&') or (i == '|') or (i == '^') or (i == '&&') or (i == '||') ):
+				operators = operators+1
+			else:
+				identifiers=identifiers+1
+		
+	print' 1) Total number of keywords:',keyword
+	print' 2) Total number of operators:',operators
+	print' 3) Total number of identifiers:',identifiers
+			
+		
+	
 f = open('geek.c','r')
 data = f.readlines()
 forkeyword(data)
-foroperator(data)
